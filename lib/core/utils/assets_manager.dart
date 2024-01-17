d2746d9825a6d8a37f5c05509194adfd272b15d7
ali sodan <alisodan.programmer@gmail.com>
2024-01-17 03:02:37 +0200

    add assets pathes file

---
+++ lib/core/utils/assets_manager.dart
@@ -0,0 +1,12 @@
+class AssetsManager{
+
+  ///lotties
+  static const String error = 'assets/lottie/error.json';
+  static const String loading = 'assets/lottie/loading.json';
+  static const String warning = 'assets/lottie/warning.json';
+  static const String noInternet = 'assets/lottie/no_internet.json';
+  static const String noDataAnimation = 'assets/lottie/no_data.json';
+  static const String success = 'assets/lottie/success.json';
+  static const String dogRunner = 'assets/gif/dog_loading.gif';
+  static const String catLoading = 'assets/gif/cat_loading.gif';
+}
